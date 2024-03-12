9a793a54bab9cc90a35a54acac714c4b3c01bae4
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 14:50:13 +0100

    feat: add for cicle snack-4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -3,7 +3,12 @@
 // - Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
 // 1 - Chiedo un numero a quattro cifre all'utente
-let userNumber = parseInt(prompt('Dimmi un numero a 4 cifre'));
-console.log('user-number' , userNumber);
+let userNumber = prompt('Dimmi un numero a 4 cifre');
+// console.log('user-number' , userNumber);
 // 2 - Leggo le 4 cifre del numero
         // - sommo le 4 cifre
+        for (let i = 0; i < userNumber.length; i++) {
+            let numbers = parseInt(userNumber[i]);
+            console.log('numbers' , numbers);
+        }
+        
