4ec5d8a68d06bac3390a07b6ae44447fa5241c69
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 14:27:49 +0100

    feat: add div+text color snack-3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,3 +1,16 @@
 // - Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+
+// 1 - Creo due div con due id diversi
+        // - assegno ad uno il colore del testo rosso e all'altro il colore del testo verde
+        let redText = document.querySelector('#red');
+        console.log('red' , redText);
+        redText.style.color = "red";
+        let greenText = document.querySelector('#green');
+        console.log('green' , greenText);
+        greenText.style.color = "green";
+// 2 - Creo un array di numeri
+        // - quelli pari li metto nell'id rosso
+        // - quelli dispari li metto nell'id verde
