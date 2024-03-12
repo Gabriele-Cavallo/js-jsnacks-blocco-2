96bddfe2a4b394a3bdfeec4f079c17a4d79fb2f2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 13:34:18 +0100

    feat: add prompt snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,2 +1,8 @@
 // - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
+
+// 1 - Chiedo al numero all'utente
+let userNumber = parseInt(prompt('Dimmi un numero'));
+console.log('user-number' , userNumber);
+// 2 - Se il numero è pari stampo il numero
+    // - altrimenti stampo il numero successivo a quello inserito
