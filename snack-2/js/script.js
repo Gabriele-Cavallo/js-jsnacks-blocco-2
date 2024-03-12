5cf795e9548fb67c17305fe07d6c07fca549d1cd
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 13:48:07 +0100

    feat: 1st method snack-2

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,2 +1,17 @@
 // - L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// 1 - Chiedo all'utente due parole
+let userWord = prompt('Dimmi una parola');
+let userWord2 = prompt('Dimmi una parola');
+// 2 - Confronto la lunghezza delle due parole
+    // - stampo prima la parola più corta e poi la più lunga
+if(userWord.length < userWord2){
+    console.log('user-word-short' , userWord);
+    console.log('user-word-long' , userWord2);
+}else{
+    console.log('user-word-long' , userWord2);
+    console.log('user-word-short' , userWord);
+}
+
+// Oppure
