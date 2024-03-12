41376687d934aa785b814cf7d1e4ed8d702ef543
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 14:39:02 +0100

    feat: add user prompt snack-4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,3 +1,9 @@
 // BONUS:
 
 // - Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+// 1 - Chiedo un numero a quattro cifre all'utente
+let userNumber = parseInt(prompt('Dimmi un numero a 4 cifre'));
+console.log('user-number' , userNumber);
+// 2 - Leggo le 4 cifre del numero
+        // - sommo le 4 cifre
