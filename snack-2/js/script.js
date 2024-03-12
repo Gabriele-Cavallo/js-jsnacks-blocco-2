730f6b19eadf7b4206bc9934f58a328376271420
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-12 14:09:14 +0100

    fix: if condition syntax

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -6,12 +6,11 @@ let userWord = prompt('Dimmi una parola');
 let userWord2 = prompt('Dimmi una parola');
 // 2 - Confronto la lunghezza delle due parole
     // - stampo prima la parola più corta e poi la più lunga
-if(userWord.length < userWord2){
+if(userWord.length < userWord2.length){
     console.log('user-word-short' , userWord);
     console.log('user-word-long' , userWord2);
 }else{
-    console.log('user-word-long' , userWord2);
-    console.log('user-word-short' , userWord);
+    console.log('user-word-short' , userWord2);
+    console.log('user-word-long' , userWord);
 }
 
-// Oppure
