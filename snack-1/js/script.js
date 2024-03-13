bccad7bfe59d2d54feec3bdfe47131e1e6d17ecd
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-13 09:42:57 +0100

    feat: add 2nd method snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,12 +2,22 @@
 // se è dispari stampa il numero successivo
 
 // 1 - Chiedo al numero all'utente
+// let userNumber = parseInt(prompt('Dimmi un numero'));
+// console.log('user-number' , userNumber);
+// // 2 - Se il numero è pari stampo il numero
+//     // - altrimenti stampo il numero successivo a quello inserito
+// if (userNumber % 2 === 0){
+//     console.log('user-number-even' , userNumber);
+// }else{
+//     console.log('user-number-odd' , userNumber + 1);
+// }
+
+// 2 - Metodo
+
 let userNumber = parseInt(prompt('Dimmi un numero'));
-console.log('user-number' , userNumber);
-// 2 - Se il numero è pari stampo il numero
-    // - altrimenti stampo il numero successivo a quello inserito
 if (userNumber % 2 === 0){
     console.log('user-number-even' , userNumber);
 }else{
-    console.log('user-number-odd' , userNumber + 1);
+    userNumber++;
+    console.log('user-number-odd' , userNumber);
 }
